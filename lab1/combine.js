451b3144dfd0aa92f73d2c5a77918d3bc541e0dc
= <poudel.yubaraj42@gmail.com>
2025-03-22 19:08:36 +0545

    initial commit

---
+++ lab1/combine.js
@@ -0,0 +1,9 @@
+import add from "./add.js";         
+import multiply from "./multiply.js"; 
+function combine() {
+    let c = add(2, 3);     
+    let d = multiply(2, 3); 
+    console.log(` ${c+d}`);    
+}
+
+combine();
